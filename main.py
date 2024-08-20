d7b35a364da4300e044b51a4d4c82441109f2580
CPirone <120253739+CPirone@users.noreply.github.com>
2024-08-20 20:31:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,42 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
-import requests
-import secrets  
-import os  
+import os
+import secrets
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 app = Flask(__name__)
+
+# Configurações da API SendGrid obtidas de variáveis de ambiente
+FIXED_RECIPIENTS = os.getenv('FIXED_RECIPIENTS').split(',')
+FIXED_FROM = os.getenv('FIXED_FROM')
+
+# Gere uma chave secreta segura (para produção, use uma chave mais complexa)
 app.secret_key = secrets.token_hex(16)
 
-MAILGUN_API_KEY = os.getenv('MAILGUN_API_KEY')
-MAILGUN_DOMAIN = os.getenv('MAILGUN_DOMAIN')
-FIXED_RECIPIENTS = os.getenv('FIXED_RECIPIENTS')
-
 def send_simple_message(name, message):
-    response = requests.post(
-        f"https://api.mailgun.net/v3/{MAILGUN_DOMAIN}/messages",
-        auth=("api", MAILGUN_API_KEY),
-        data={
-            "from": f"Excited User <mailgun@{MAILGUN_DOMAIN}>",
-            "to": FIXED_RECIPIENTS,
-            "subject": f"Hello {name}!",
-            "text": f"Hi {name},\n\n{message}"
-        }
+    message = Mail(
+        from_email=FIXED_FROM,
+        to_emails=FIXED_RECIPIENTS,
+        subject=f'Hello {name}!',
+        html_content=f'<strong>{message}</strong>'
     )
-    print(f"Response Status Code: {response.status_code}")
-    print(f"Response Text: {response.text}")
-    return response
+    try:
+        sg = SendGridAPIClient(os.getenv('SENDGRID_API_KEY'))
+        response = sg.send(message)
+        return response
+    except Exception as e:
+        print(e.message)
+        return None
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         name = request.form['name']
-       
         response = send_simple_message(name, "Obrigado pelo submit na aula 70!")
-        if response.status_code == 200:
-            flash('Email enviado!', 'Sucesso')
+        if response and response.status_code == 202:  # SendGrid retorna 202 para sucesso
+            flash('Email enviado!', 'success')
         else:
-            flash('Falha ao enviar e-mail. Por favor, tente mais tarde.', 'Perigo')
+            flash('Falha ao enviar e-mail. Por favor, tente mais tarde.', 'danger')
         session['name'] = name
         return redirect(url_for('index'))
 
